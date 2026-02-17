7d049a1f9c21420f964026cc31171900cbd1e19f
Elion Behluli <elionbehluli7@gmail.com>
2026-02-17 20:53:56 +0100

    show products on front

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Illuminate\Support\Facades\Route;
+
+class ProductController extends Controller
+{
+    public function index()
+    {
+        return Inertia::render('Welcome', [
+            'products' => Product::with('images')->latest()->get(),
+            'canLogin' => Route::has('login'),
+            'canRegister' => Route::has('register'),
+        ]);
+    }
+}
